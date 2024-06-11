352ea0add99dca5878177e07162a665d5d9b0675
enigmaticenigma <82358965+enigmaticenigma@users.noreply.github.com>
2024-06-11 10:32:43 +1000

    feat: add delete mutations for atc + flightplans

---
+++ src/app/actions/deleteATC.ts
@@ -0,0 +1,19 @@
+import { db } from "@/lib/db"
+import { atcs } from "@/lib/schema"
+import { eq } from "drizzle-orm"
+
+export async function deleteATC({ atcId }: { atcId: number }) {
+  if (!atcId) {
+    console.error("Invalid data")
+    return false
+  }
+
+  try {
+    await db.delete(atcs).where(eq(atcs.id, atcId))
+    return true
+  } catch (error) {
+    console.error(error)
+    return false
+  }
+}
+
