bd82668ef791d867ecb9ce15216dcee293b25fc5
enigmaticenigma <82358965+enigmaticenigma@users.noreply.github.com>
2024-06-11 18:01:46 +1000

    fix: Promise<Boolean> type error

--- src/components/PilotUpdateForm.tsx
+++ src/components/PilotUpdateForm.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button"
 import { deleteFlightplan } from "@/app/actions/deleteFlightplan"
 import { updateFlightplan } from "@/app/actions/updateFlightplan"
 import { Flightplan } from "@/types/flightplan"
+import { useCallback } from "react"
 
 const PilotSchema = z.object({
   squawk: z.coerce.number(),
@@ -26,13 +27,17 @@ export default function PilotUpdateForm({ flightplan }: { flightplan: Flightplan
     await updateFlightplan({ ...values, flightplan })
   }
 
+  const onClick = useCallback(async () => {
+    await deleteFlightplan({ flightplanId: flightplan.id })
+  }, [flightplan.id]);
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="grid grid-cols-2 gap-4">
         <FormInput form={form} label={"Squawk"} type={"number"} id={"squawk"} />
         <FormInput form={form} label={"Frequency"} type={"text"} id={"frequency"} />
         <Button type={"submit"} className="col-span-2">Submit</Button>
-        <Button type="button" className="col-span-2" onClick={deleteFlightplan({ flightplanId: flightplan.id })}>Delete</Button>
+        <Button type="button" className="col-span-2" onClick={onClick}>Delete</Button>
       </form>
     </Form>
   )
