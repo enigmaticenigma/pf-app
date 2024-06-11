c5e11ae657887f458b86c63f807e2cc9fce813fd
enigmaticenigma <82358965+enigmaticenigma@users.noreply.github.com>
2024-06-11 12:27:34 +1000

    fix: JS-0240

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -3,13 +3,13 @@ import { Flightplan } from "@/types/flightplan"
 import useSWR from "swr"
 
 export function useFlightplansByFrequency(frequency: number) {
-  return useSWR(["/api/flightplans", { frequency: frequency }])
+  return useSWR(["/api/flightplans", { frequency }])
 }
 
 export function useATC(atcId: number) {
-  return useSWR<ATCData, unknown>(["/api/atc", { atcId: atcId }])
+  return useSWR<ATCData, unknown>(["/api/atc", { atcId }])
 }
 
 export function useFlightplan(flightplanId: number) {
-  return useSWR<Flightplan, unknown>(["/api/flightplan", { flightplanId: flightplanId }])
+  return useSWR<Flightplan, unknown>(["/api/flightplan", { flightplanId }])
 }
